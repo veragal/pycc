056acd79904b31ae8d8922d5e6bbd525d2848ff9
Vera <galstyanvera@gmail.com>
2018-01-31 18:12:47 -0500

    functions

---
+++ chp8/8_4large_shirts.py
@@ -0,0 +1,7 @@
+#  Copyright (c) Vera Galstyan Jan 25,2018
+def make_shirt(size='large',message='I love python'):
+	print("my shirt is " + size + " and has a print-" + message)
+
+make_shirt()
+make_shirt(size='medium')
+make_shirt('small','I like C')
