0fcc6336edd8fcf310694467264b38832df5944c
Vera <galstyanvera@gmail.com>
2018-01-31 18:12:37 -0500

    functions with default values

---
+++ chp8/8_3t_shirt.py
@@ -0,0 +1,5 @@
+#  Copyright (c) Vera Galstyan Jan 25,2018
+def make_shirt(size,message):
+	print("This is your size " + size + " and the message on your shirt is " + message)
+make_shirt('medium','love')	
+make_shirt(size = 'large',message ='will you marry me')
