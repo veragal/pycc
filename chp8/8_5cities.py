001635d278ac7681f6fe97fbf5d1aa010e2bde04
Vera <galstyanvera@gmail.com>
2018-01-31 18:13:12 -0500

    functions-argument and parameter

---
+++ chp8/8_5cities.py
@@ -0,0 +1,7 @@
+#  Copyright (c) Vera Galstyan Jan 25,2018
+def describe_city(name,country='france'):
+	print(name + " is in " + country)
+
+describe_city('paris')
+describe_city(name= 'berlin',country = 'germany')
+describe_city(name='madrid',country = 'spain')
