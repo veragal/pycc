441204f562823c5d411014576b42a8dc1becf18c
Vera <galstyanvera@gmail.com>
2018-02-02 19:50:37 -0500

    function

---
+++ chp8/8_9magicians.py
@@ -0,0 +1,7 @@
+#  Copyright (c) Vera Galstyan Jan 25,2018
+def show_magicians():
+	for magician in magicians:
+		print("Hello magian " + magician)
+
+magicians=['vera','ofa','lia']
+show_magicians()
